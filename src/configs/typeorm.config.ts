c9ae270ccacdb756e29e7af0f2e15968daddbb7d
robert <robert@robertui-MacBookPro.local>
2025-03-19 18:55:16 +0900

    add config

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -1,12 +1,15 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as config from 'config';
 
+const dbConfig = config.get('db');
+    
 export const TypeORMConfig : TypeOrmModuleOptions = {
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: 'root',
-    database: 'board_app',
+    type: dbConfig.type,
+    host: process.env.RDS_HOSTNAME || dbConfig.host,
+    port: process.env.RDS_PORT || dbConfig.port,
+    database: process.env.RDS_DB_NAME || dbConfig.database,
+    username: process.env.RDS_USERNAME || dbConfig.username,
+    password: process.env.RDS_USERNAME || dbConfig.password,
     entities: [__dirname + '/../**/*.entity.{js,ts}'],
-    synchronize: true,
+    synchronize: dbConfig.synchronize,
 }
